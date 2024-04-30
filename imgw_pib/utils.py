3ce5ff4d9671dde00d167b34cfe9b79bb7e31711
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-04-30 09:57:16 +0200

    Add logging error

--- imgw_pib/utils.py
+++ imgw_pib/utils.py
@@ -1,6 +1,9 @@
 """Utils for imgw-pib."""
 
+import logging
 from datetime import UTC, datetime
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def gen_station_name(station: str, river: str) -> str:
@@ -16,4 +19,5 @@ def get_datetime(date_time: str, date_format: str) -> datetime | None:
     try:
         return datetime.strptime(date_time, date_format).replace(tzinfo=UTC)
     except (TypeError, ValueError):
+        _LOGGER.debug("Invalid date-time string: %s", date_time)
         return None
