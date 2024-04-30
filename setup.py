aa2a15b8da39cedbeb9058364b53721504f3601b
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-04-30 10:16:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.0.5"
+VERSION = "1.0.0"
 
 setup(
     name="imgw_pib",
@@ -23,6 +23,7 @@ setup(
     python_requires=">=3.12",
     install_requires=["aiohttp>=3.8.0"],
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
